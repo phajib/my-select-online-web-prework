f2140b5ff965c0d938eb784a583dd9ade5aabf4f
H B <phajib@gmail.com>
2019-01-22 07:44:46 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -5,24 +5,8 @@ def my_select(collection)
  while i < collection.length
    if (yield(collection[i]))
      new_array.push(collection[i])
-   end #yield (collection[i])
+   end
      i += 1
  end
  new_array
 end
-
-
-=begin
-def my_select(array)
-  i =0
-  select = []
-  while i < array.length
-    if (yield(array[i]))
-      # select.push(array[i])OR
-      select << array[i]
-    end
-    i += 1
-  end
-  select
-end
-=end
